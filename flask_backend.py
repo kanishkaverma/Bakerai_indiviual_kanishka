f72921fa4b1b0a5a2a27c9c42ea1b24bd355576c
kanishkaverma <kanishkaverma@users.noreply.github.com>
2021-04-14 07:42:43 +0530

    final commit

--- flask_backend.py
+++ flask_backend.py
@@ -1,27 +1,56 @@
 from flask import Flask
 from flask import Flask,render_template,request
-from main import start 
+from numpy.core.records import fromrecords
+from main import start
+import datetime 
+import json 
 
+frequency = { } 
+def init_freq(): 
+     for i in range(24): 
+         frequency[i] = 0; 
+
+
+
+init_freq()
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index(): 
-    bot_response = chat_bot()
-    return render_template("index.html", bot_response=bot_response) 
+    now = datetime.datetime.now()
+    # print(now.hour, now.minute)
+    if now.hour == 0 and now.minute ==0: 
+        frequency.clear()
+        init_freq()
+    if now.hour in frequency: 
+
+        frequency[now.hour] += 1 
+    else: 
+
+        frequency[now.hour ] = 0 
+    
+    # print(frequency)
+    return render_template("index.html") 
 
 @app.route("/get")
 def chat_bot(): 
     user_input = request.args.get("user-input")
     print(user_input)
+
+   
     if request.method == "GET" and user_input != None:
         print('request_received')
-        # print(user_input)
         bot_response = start(user_input)
         print(bot_response)
         return bot_response 
     return "test_response"
 
+@app.route("/frequency")
+def freq(): 
+   
+    json_object = json.dumps(frequency, indent = 4)  
+    return json_object 
 if __name__ == "__main__": 
     app.run(debug=True)
